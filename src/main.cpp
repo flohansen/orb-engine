0c5989d92f43d5805b7fc1e0e778b9f68f066a9f
Florian Hansen <flhansen.93@gmail.com>
2024-05-11 14:26:34 +0200

    feat: create basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <GLFW/glfw3.h>
+#include <cstddef>
+#include <cstdio>
+
+void handle_error(int error_code, const char* description);
 
 int main() {
     if (!glfwInit()) {
         return -1;
     }
+
+    glfwSetErrorCallback(handle_error);
+    GLFWwindow* wnd = glfwCreateWindow(800, 600, "Game", NULL, NULL);
+    if (!wnd) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(wnd);
+    while (!glfwWindowShouldClose(wnd)) {
+    }
+
+    glfwDestroyWindow(wnd);
+    glfwTerminate();
+}
+
+void handle_error(int error_code, const char* description) {
+    fprintf(stderr, "[GLFW] Error (code %d): %s\n", error_code, description);
 }
